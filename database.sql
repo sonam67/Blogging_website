182347cf71e6494718a9dead6c6bfd2aeac9b8da
Sonam Vitthal <90518624+sonam67@users.noreply.github.com>
2022-09-18 11:32:14 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,30 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+ble structure for table `users`
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(8, 'sonam', 'sonam@gmail.com', '0139a3c5cf42394be982e766c93f5ed0');
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
